a25d938fee4b63fdcfd1c5235ee9f307bf455a60
Anthony A <asanderson2009@gmail.com>
2019-06-09 20:35:19 -0400

    on click works for images for crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,58 @@
 $ (window).on("load", function() {
      alert("it works");
 });
+//Global Variables
+
+// Crystal variable, using sub-objects of color
+var crystal = {
+red: 
+{
+    name: "Red",
+    value: 0
+},
+
+blue: 
+{
+    name: "Blue",
+    value: 0
+},
+
+yellow:
+{
+    name: "Yellow",
+    value: 0
+},
+
+green: 
+{
+    name: "Green",
+    value: 0
+}
+
+}
+
+
+
+// Functions
+// Score
+var winningScore = 0
+var playerScore  = 0
+
+var win  = 0 
+var loss = 0
+
+$("#red").on("click", function(){
+    alert("test");
+});
+
+$("#blue").on("click", function(){
+    alert("test");
+});
+
+$("#yellow").on("click", function(){
+    alert("test");
+});
+
+$("#green").on("click", function(){
+    alert("test");
+});
